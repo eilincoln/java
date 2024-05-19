af2afaeb620988f242f850a285fcc9ed6124a1a2
eilincoln <lincoln.santospro@gmail.com>
2024-05-19 16:18:33 -0300

    adicionando operacoes com numeros inteiros e com numeros double

---
+++ workspace-java-course/.metadata/.plugins/org.eclipse.core.resources/.history/ff/809e49311416001f1a4f878a3c0190b1
@@ -0,0 +1,14 @@
+package executable.javacourse;
+
+public class FazendoOperacoesEntreNumerosInteiros {
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Sua média é: " + (nota1 + nota2 + nota3 + nota4) / 4);
+		
+	}
+}
---
+++ workspace-java-course/first-java-program/src/executable/javacourse/FazendoOperacoesEntreNumerosInteiros2.java
@@ -0,0 +1,17 @@
+package executable.javacourse;
+
+public class FazendoOperacoesEntreNumerosInteiros2 {
+	public static void main(String[] args) {
+		
+		//Neste caso trabalhando com double para que fique com todas as casas decimais
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("Sua média é: " + mediaFinal);
+		
+	}
+}
