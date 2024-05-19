844736eb643878d32fbb7a112897473496a9e3af
eilincoln <lincoln.santospro@gmail.com>
2024-05-19 14:03:20 -0300

    running my first command lines

---
+++ workspace-java-course/.metadata/.plugins/org.eclipse.core.resources/.history/e3/30c34d940016001f1a4f878a3c0190b1
@@ -0,0 +1,10 @@
+package java.executable;
+
+public class FirstClassJava {
+
+	// main é um método auto executavel em java
+	public static void main(String[] args) {
+		System.out.println("my first java program");
+	}
+
+}
---
+++ workspace-java-course/first-java-program/src/executable/javacourse/FirstClassJava.java
@@ -0,0 +1,13 @@
+package executable.javacourse;
+
+public class FirstClassJava {
+
+	// main is a self-executable method in java
+	public static void main(String[] args) {
+		
+		//write the message to the console
+		System.out.println("my first java program");
+		System.out.println("java is object oriented");
+	}
+
+}
