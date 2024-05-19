1f5b66d59a34e32684a74b7309c7bc6bab30b0c4
eilincoln <lincoln.santospro@gmail.com>
2024-05-19 15:13:26 -0300

    more about variables

---
+++ workspace-java-course/first-java-program/src/executable/javacourse/LocalAndGlobalVariables.java
@@ -0,0 +1,19 @@
+package executable.javacourse;
+
+public class LocalAndGlobalVariables {
+	
+	int globalAge = 45; //this var works for all methods
+	
+	public static void main(String[] args) {
+		// a local variable is when I declare it within a method
+		int age = 18;
+		System.out.println("your age is: " + age);
+	}
+	
+	public static void method2() {
+		//this variable only works here
+		int anotherAge = 34;
+		System.out.println("your age is: " + anotherAge);
+	}
+
+}
