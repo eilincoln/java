c6bcc8b86dbea93948ea6789da12e6ff452ad9b0
eilincoln <lincoln.santospro@gmail.com>
2024-05-20 11:00:21 -0300

    add os comentários e aprendo sobre referência e instância

--- workspace-java-course/first-java-program/src/executable/javacourse/CriandoAluno.java
+++ workspace-java-course/first-java-program/src/executable/javacourse/CriandoAluno.java
@@ -1,5 +1,17 @@
 package executable.javacourse;
 
-public class CriandoAluno {
+import classes.javacourse.Aluno;
 
+public class CriandoAluno {
+	public static void main(String[] args) {
+		//Criando objeto através da classe
+		
+		// new Aluno(); -> é uma instância (É a criação de um objeto literal)
+		// quando eu crio o aluno1 ou os outros, eu faço uma referência a classe aluno, que é um objeto
+		Aluno aluno1 = new Aluno(); //Criado sem parâmetro
+		
+		Aluno aluno2 = new Aluno("Lincoln"); //criado com 1 parâmetro
+		
+		Aluno aluno3 = new Aluno("Lincoln2", 25); // Criando com 2 parâmetros
+	}
 }
