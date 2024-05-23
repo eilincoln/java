a4555ee6446129e7282cde79efbf595d8024f841
eilincoln <lincoln.santospro@gmail.com>
2024-05-23 11:21:07 -0300

    adicionando programa funcional em programação a obj

--- eclipse-workspace/programacao-orientada-obj/src/cursoJava/Metodos.java
+++ eclipse-workspace/programacao-orientada-obj/src/cursoJava/Metodos.java
@@ -1,36 +1,45 @@
 package cursoJava;
 
+import javax.swing.JOptionPane;
+
 import classes.Aluno;
 
 public class Metodos {
 	public static void main(String[] args) {
 		
-		Aluno aluno01 = new Aluno("Lincoln", 202208254);
-		aluno01.setCurso("Engenharia de Software");
-		aluno01.setNomeMae("Fabiana Semiano");
+		/*Recebendo dados*/
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno: ");
+		String ra = JOptionPane.showInputDialog("Qual o RA: ");	
+		String curso = JOptionPane.showInputDialog("Qual curso: ");
+		String nomeMae = JOptionPane.showInputDialog("Qual é o nome da mãe: ");
+		double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 1: "));
+		double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 2: "));
+		double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 3: "));
+		double nota4 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 4: "));
 		
-		aluno01.setNota1(90);
-		aluno01.setNota2(80.2);
-		aluno01.setNota3(40.5);
-		aluno01.setNota4(79.3);
-		System.out.println("Méria do aluno: " + aluno01.getNome() + " é: " + aluno01.getMediaNota());
-		System.out.println("Resultado: " + (aluno01.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		// instanciando dados recebidos no obj
+		Aluno aluno01 = new Aluno(nome, Integer.valueOf(ra));
+		aluno01.setCurso(curso);
+		aluno01.setNomeMae(nomeMae);
+		aluno01.setNota1(nota1);
+		aluno01.setNota2(nota2);
+		aluno01.setNota3(nota3);
+		aluno01.setNota4(nota4);
+		
+		System.out.println("Aluno: " + aluno01.getNome());
+		System.out.println("RA: " + aluno01.getRa());
+		System.out.println("Curso: " + aluno01.getCurso());
+		System.out.println("Nome da mãe: " + aluno01.getNomeMae());
+		System.out.println("Media: " + aluno01.getMediaNota());
+		System.out.println("Resultdo: " + (aluno01.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 		
 		
-		System.out.println("O aluno: " + aluno01.getNome() + " tem o RA: " + aluno01.getRa());
-		System.out.println("Faz o curso: " + aluno01.getCurso() + " e o nome da mãe é: " + aluno01.getNomeMae());
+		
+//		System.out.println("Méria do aluno: " + aluno01.getNome() + " é: " + aluno01.getMediaNota());
+//		System.out.println("Resultado: " + (aluno01.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+//		System.out.println("O aluno: " + aluno01.getNome() + " tem o RA: " + aluno01.getRa());
+//		System.out.println("Faz o curso: " + aluno01.getCurso() + " e o nome da mãe é: " + aluno01.getNomeMae());
 		
 		/*===============================================================================*/
-		
-		
-		Aluno aluno02 = new Aluno();
-		aluno02.setNome("Carlos da Silva");
-//		System.out.println("Nome do aluno set é: " + aluno02.getNome());
-		
-		
-		
-		
-		//System.out.println(aluno01.nome);
-		
 	}
 }
